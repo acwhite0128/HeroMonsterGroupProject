a88a1b06faac33217a6688d733d7bc9e0bcc07a0
April White <acw7323@live.missouristate.edu>
2019-11-23 19:03:41 -0600

    Add project files.

---
+++ HeroMonsterGroupProject/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroMonsterGroupProject
+{
+    class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public bool AttackBonus { get; set; }
+
+        public Character(string name, int health, int damageMaximum, bool attackBonus)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Health = health;
+            DamageMaximum = damageMaximum;
+            AttackBonus = attackBonus;
+        }
+
+        public int Attack(Dice dice)
+        {
+            return dice.Roll(this.DamageMaximum);
+        }
+
+        public void Defend(int damage)
+        {
+            this.Health -= damage;
+        }
+
+    }
+}
---
+++ HeroMonsterGroupProject/Dice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroMonsterGroupProject
+{
+    class Dice
+    {
+        public int Sides { get; set; }
+
+        Random random = new Random();
+
+        public int Roll(int sides)
+        {
+            return random.Next(sides);
+        }
+    }
+}
+}
